a5de78ecc25830120960f30a6351aece9466ccec
Mishgunoo <140645404+Mishgunoo@users.noreply.github.com>
2023-08-08 17:27:25 +0300

    Add files via upload
    ДЗ ОТ 08.08.23 JAVA

---
+++ Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        } char myLetter = 'G';
+        int myNum = 89;
+        byte myByte = 4;
+        short Numtvo = 56;
+        float Numthree = 4.7333436F;
+        double Numfor = 4.355453532;
+        long Numfive = 12121L;
+
+        System.out.println("345"+":" + "3,4,5");
+    }
+}
